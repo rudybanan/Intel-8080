ee6df1d8a5b664b32c205ff2901533a4dbf702df
Hubert <113052810+rudybanan@users.noreply.github.com>
2022-10-20 16:13:47 +0200

    adding XCHG instruction and formating script

--- Simulator.py
+++ Simulator.py
@@ -10,14 +10,14 @@ registers = {"AL": None,
 
 
 def register_state():
-    print("\nInitial state of the registers of Intel 8080 processor: \n")
+    print("\nInitial state of the registers of Intel 8086 processor: \n")
     for register in registers:
         print(register, "=", registers[register])
 
 
 def user_input():
     for r in registers:
-        registers[r] = input(f"Enter value stored in {r} register: ")
+        registers[r] = input(f"Enter value stored in {r} register: ").upper()
 
 
 # noinspection PyTypeChecker
@@ -32,32 +32,45 @@ def MOV(a, b):
     registers[a] = registers[b]
 
 
+def XCHG(x, y):
+    registers[x], registers[y] = registers[y], registers[x]
+
+
 run = True
 while run:
     register_state()
-    action = int(input(
-        "\nEnter action you want to execute:\n\nChange values of the registers - 1\nEnter instruction for program to execute - 2\nQuit - 3\n"))
-    if action == 1:
-        wrong_inputs = True
-        while wrong_inputs:
-            user_input()
-            if inputs_hex_and_8_bit():
-                wrong_inputs = False
+    try:
+        action = int(input("\nEnter action you want to execute:\n\nChange values of the registers - 1\nEnter instruction for program to execute - 2\nQuit - 3\n\n"))
+        if action == 1:
+            wrong_inputs = True
+            while wrong_inputs:
+                user_input()
+                if inputs_hex_and_8_bit():
+                    wrong_inputs = False
+                else:
+                    print("\nInputs not hexadecimal or not 8 bit!")
+        elif action == 2:
+            instruction = int(input("\nChoose instruction for simulation:\nMOV - 1\nXCHG - 2\n\n"))
+            if instruction == 1:
+                reg1 = input("Enter first register for MOV instruction: ").upper()
+                reg2 = input("Enter second register for MOV instruction: ").upper()
+                if reg1 and reg2 in registers:
+                    MOV(reg2, reg1)
+                    print("\n")
+                else:
+                    print("\nWrong registers!\n")
+            elif instruction == 2:
+                reg1 = input("Enter first register for XCHG instruction: ")
+                reg2 = input("Enter second register for XCHG instruction: ")
+                if reg1 and reg2 in registers:
+                    XCHG(reg1, reg2)
+                else:
+                    print("\nWrong registers!\n")
             else:
-                print("\nInputs not hexadecimal or not 8 bit!")
-    elif action == 2:
-        instruction = input("\nEnter instruction name for simulation: ")
-        if instruction == "MOV":
-            reg1 = input("Enter first register for MOV instruction: ")
-            reg2 = input("Enter second register for MOV instruction: ")
-            if reg1 and reg2 in registers:
-                MOV(reg2, reg1)
-                print("\n")
-            else:
-                print("\nWrong registers!\n")
+                print("Wrong instruction!")
+        elif action == 3:
+            run = False
         else:
-            print("Wrong instruction!")
-    elif action == 3:
-        run = False
-    else:
+            print("Wrong action!")
+    except ValueError:
         print("Wrong action!")
